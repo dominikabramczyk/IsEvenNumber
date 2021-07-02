cee9d64d7fc4bcdd822509a2f94506b20db49db5
dominikabramczyk <dominik.abramczyk.1@gmail.com>
2021-07-02 10:19:52 +0200

    Dodaj pliki projektów.

---
+++ thirdHomework/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isEven = IsEven();
+            if (isEven)
+            {
+                Console.WriteLine("Twoja liczba jest parzysta");
+            }
+            else
+            {
+                Console.WriteLine("Twoja liczba nie jest parzysta.");
+            }
+        }
+
+        public static int LoadingNumber() //wczytywanie danych od użytkownika
+        {
+            var userNumber = 0;
+            try
+            {
+                Console.WriteLine("Podaj swoja liczbe.");
+                userNumber = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex) //obsługiwanie błędów
+            {
+                Console.WriteLine("Nieprawidłowe dane");
+                Console.ReadLine();
+                Environment.Exit(1);
+               
+            }
+            return userNumber;
+        }
+
+        public static bool IsEven() //czy parzysta
+        {
+            var userNumber = LoadingNumber();
+            if (userNumber % 2 == 0) //jest parzysta
+            {
+                return true;
+            }
+            else //nie jest parzysta
+            {
+                return false;
+            }
+        }
+
+    }
+}
